c7cb55e4b484ae4302e797d902571b1c4e43c4e4
Mauro Cunha <cunhamauro@outlook.pt>
2024-07-22 17:14:17 +0100

    InitProject

---
+++ Sales_NET8.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sales_NET8.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
