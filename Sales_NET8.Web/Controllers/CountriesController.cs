69cb511f159811372dac29705e0f42ad41bf637e
Mauro Cunha <cunhamauro@outlook.pt>
2024-07-22 17:25:39 +0100

    Controllers created.

---
+++ Sales_NET8.Web/Controllers/CountriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sales_NET8.Web.Controllers
+{
+    public class CountriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
